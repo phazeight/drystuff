57533dbdc6f01f65e1f81d540c4aa76dfd021bb9
Luke Thompson <lucas.thompson@uptake.com>
2023-04-18 12:11:59 -0400

    initial single vpc
    updates
    
    rm crap

---
+++ outputs.tf
@@ -0,0 +1,17 @@
+## VPC
+
+# output "vpc_id" {
+#   value = module.vpc_stuff.vpc_id
+# }
+
+# output "vpc_cidr_block" {
+#   value = module.vpc_stuff.vpc_cidr_block
+# }
+
+# output "public_subnets_id" {
+#   value = module.vpc_stuff.public_subnets
+# }
+
+# output "private_subnets_id" {
+#   value = module.vpc_stuff.private_subnets
+# }
